1265d034a1cc597a10f00ab5832d4ef98a469246
imranshaik2263 <imranshaik2263@gmail.com>
2026-01-17 12:58:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+    if (cardTop < window.innerHeight - 100) {
+      card.classList.add('show');
+    }
+  });
+});
+
+
+document.getElementById("darkToggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
